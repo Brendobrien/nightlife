e6130924ed95c3f5c6016885c24d2067f2e6f690
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-10-05 11:46:13 +0700

    Cleaned up Foundation

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express'),
 app.route('/')
 .get((req, res)=> res.sendFile(process.cwd() + '/index.html'));
 
+app.route('/signin')
+.get((req, res)=> res.sendFile(process.cwd() + '/signin.html'));
+
+app.route('/signup')
+.get((req, res)=> res.sendFile(process.cwd() + '/signup.html'));
 
 var port = process.env.PORT || 3003;
 // const server = http.createServer(app);
