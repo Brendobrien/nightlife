b317b7dc1c1f6c72c8c48ec9d186eb2db00cbb33
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-10-05 17:37:05 +0700

    pictures look decent now

--- api.js
+++ api.js
@@ -1,18 +1,20 @@
 var request = require("request");
 
 module.exports.yelp = function(req,res) {
-  var options = { method: 'GET',
-  url: 'https://api.yelp.com/v3/businesses/search',
-  qs: { term: 'bar', location: 'Dallas', limit: '9' },
-  headers: 
-   { 'postman-token': '3aa99da9-cb88-0451-193e-d0ab04e5a40c',
-     'cache-control': 'no-cache',
-     authorization: 'Bearer 04j_jr5avF7wL43fyKU00SxvliXA9672KocecDCWu8PDxXv-bUgaXcjdNL8sMDFG19HWjWhhIkhi42GtY_k547zAtx6KLGB1NSkvliyWjYAdH57V5FaIKtX_HbnzV3Yx',
-     'content-type': 'application/json' } };
-  
-  request(options, function (error, response, body) {
+  var bars = {
+    method: 'GET',
+    url: 'https://api.yelp.com/v3/businesses/search',
+    qs: { term: 'bar', location: req.query.city, limit: '21' },
+    headers: { 
+      'authorization': 'Bearer 04j_jr5avF7wL43fyKU00SxvliXA9672KocecDCWu8PDxXv-bUgaXcjdNL8sMDFG19HWjWhhIkhi42GtY_k547zAtx6KLGB1NSkvliyWjYAdH57V5FaIKtX_HbnzV3Yx',
+      'content-type': 'application/json'
+    } 
+  };
+
+  request(bars, function (error, response, body) {
     if (error) throw new Error(error);
 
-    res.send(body);
+    var json = JSON.parse(body);
+    res.send(json.businesses);
   });
 }
