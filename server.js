5837267ce600b4a5674210bb2912cddc21beff51
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-10-05 10:12:23 +0700

    let's git it going:

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express'),
-  const app = express();
+  app = express();
+
+app.route('/')
+.get((req, res)=> res.sendFile(process.cwd() + '/index.html'));
+
+
+var port = process.env.PORT || 3003;
+// const server = http.createServer(app);
+var server = app.listen(port, function(){
+  console.log("Server listening on port "+port);
+});
