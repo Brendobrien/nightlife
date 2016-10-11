d855084bf9011b0e0116f9e1cd26880ab83d9016
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-10-11 08:27:41 +0700

    it harms it harms me it harms, ill let it in

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ const router = require('./router.js');
 
 // DB Setup
 // mongoose.connect(process.env.MONGO_URI);
-mongoose.connect('mongodb://localhost:auth/auth');
+require('dotenv').load(),
+mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
-require('dotenv').load(),
 app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
@@ -21,6 +22,7 @@ router(app);
 
 // Front-End 
 app.use(express.static('public'));
+
 
 var port = process.env.PORT || 3003;
 var server = app.listen(port, function(){
